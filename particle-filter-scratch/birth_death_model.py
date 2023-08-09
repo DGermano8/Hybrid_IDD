9d9231815f197ce353ca20696ba6edd98cc1d839
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-08-09 14:51:45 +1000

    add a ctmc solver class

--- particle-filter-scratch/birth_death_model.py
+++ particle-filter-scratch/birth_death_model.py
@@ -7,9 +7,15 @@ from pypfilt.obs import Univariate, Obs
 
 # --------------------------------------------------------------------
 # Define the process models
+#
+# - BirthDeathODE :: ODE
+# - BirthDeathSDE :: SDE
+# - BirthDeathCTMC :: CTMC
+# - BirthDeathHybrid :: Hybrid-CTMC-ODE
+#
 # --------------------------------------------------------------------
 
-class BirthDeathEuler(Model):
+class BirthDeathODE(Model):
     def field_types(self, ctx):
         """
         Define the state that is used to completely specify a
@@ -40,7 +46,7 @@ class BirthDeathEuler(Model):
         curr['x'] = prev['x'] + time_step.dt * deriv
 
 
-class BirthDeathNoisy(Model):
+class BirthDeathSDE(Model):
     def field_types(self, ctx):
         """
         Define the state that is used to completely specify a
@@ -71,6 +77,63 @@ class BirthDeathNoisy(Model):
         diff = (prev['birthRate'] - prev['deathRate']) * prev['x'] * time_step.dt
         wein = np.sqrt(diff) * rng.normal(size=prev['x'].shape)
         curr['x'] = np.clip(prev['x'] + diff + wein, 0, None)
+
+
+class BirthDeathCTMC(Model):
+    def field_types(self, ctx):
+        return [('birthRate', np.dtype(float)),
+                ('deathRate', np.dtype(float)),
+                ('x', np.dtype(int)),
+                ('next_event', np.int_),
+                ('next_time', np.float_)]
+
+    def init(self, ctx, vec):
+        prior = ctx.data['prior']
+        vec['x'] = prior['x']
+        vec['birthRate'] = prior['birth']
+        vec['deathRate'] = prior['death']
+        vec['next_time'] = 0
+        vec['next_event'] = 0
+        self.select_next_event(ctx, vec, stop_time=0)
+
+    def update(self, ctx, time_step, is_forecast, prev, curr):
+        curr[:] = prev[:]
+        active = self.active_particles(curr, time_step.end)
+        while any(active):
+            births = np.logical_and(active, curr['next_event'] == 0)
+            curr['x'][births] += 1
+            deaths = np.logical_and(active, curr['next_event'] == 1)
+            curr['x'][deaths] -= 1
+            self.select_next_event(ctx, curr, stop_time=time_step.end)
+            active = self.active_particles(curr, time_step.end)
+
+    def active_particles(self, vec, stop_time):
+        return np.logical_and(
+            vec['next_time'] <= stop_time,
+            vec['x'] > 0,
+        )
+
+    def select_next_event(self, ctx, vec, stop_time):
+        active = self.active_particles(vec, stop_time)
+        if not any(active):
+            return
+
+        x = vec['x'][active]
+        birth = vec['birthRate'][active]
+        death = vec['deathRate'][active]
+
+        birth_rate = birth * x
+        death_rate = death * x
+        rate_sum = birth_rate + death_rate
+
+        rng = ctx.component['random']['model']
+        dt = - np.log(rng.random(x.shape)) / rate_sum
+        vec['next_time'][active] += dt
+
+        threshold = rng.random(x.shape) * rate_sum
+        death_event = threshold > birth_rate
+        vec['next_event'][active] = death_event.astype(np.int_)
+
 
 # --------------------------------------------------------------------
 # Define the observation models
