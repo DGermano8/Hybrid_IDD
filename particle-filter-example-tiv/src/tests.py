9826c99b6c04f41f29a77c603465c9704fd15ab0
aezarebski <aezarebski@gmail.com>
2023-08-27 19:15:17 +1000

    clean up test case to make this easier to understand

--- particle-filter-example-tiv/src/tests.py
+++ particle-filter-example-tiv/src/tests.py
@@ -3,21 +3,26 @@ import scipy.stats as stats
 import numpy as np
 
 
-class TestScipyStats(unittest.TestCase):
+class TestScipyLognormal(unittest.TestCase):
+    """
+    The scipy.stats.lognorm API requires specifying mu and sigma in a
+    strange way.
+    """
 
     def setUp(self):
-        self.test_data = np.array([1, 2, 3, 4, 5])
+        self.pdf = lambda x, mu, sig: np.exp(- (np.log(x) - mu)**2 / (2 * sig**2)) / (x * sig * np.sqrt(2 * np.pi))
+        self.x = np.array([1E-6, 1.0, 2.0, 5.0, 6.789])
+        self.th1 = {'mu': 1.0, 'sigma': 1.0}
+        self.th2 = {'mu': 2.0, 'sigma': 2.3}
+        self.parameter_sets = [self.th1, self.th2]
 
-    def test_mean(self):
-        self.assertEqual(np.mean(self.test_data), 3.0)
+    def test_pdf(self):
+        for th in self.parameter_sets:
+            scipy_vals = stats.lognorm.pdf(self.x, th['sigma'], scale = np.exp(th['mu']))
+            my_vals = self.pdf(self.x, th['mu'], th['sigma'])
 
-    def test_min_max(self):
-        self.assertEqual(np.min(self.test_data), 1)
-        self.assertEqual(np.max(self.test_data), 5)
-
-    def test_bogus(self):
-        with self.assertRaises(AttributeError):
-            self.test_data.fake()
+            for ix in range(len(self.x)):
+                self.assertAlmostEqual(scipy_vals[ix], my_vals[ix])
 
 
 if __name__ == '__main__':
