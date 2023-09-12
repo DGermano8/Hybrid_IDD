b6cf0b342b0d03142f0642bdd273f6589a3c096c
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-09-12 15:21:51 +1000

    include state plot figure

--- particle-filter-example-tiv/run-inf-cli.py
+++ particle-filter-example-tiv/run-inf-cli.py
@@ -39,7 +39,8 @@ def read_cli_args() -> Dict[str, Any]:
     parser.add_argument('--input_toml', required=True, help='Input TOML file')
     parser.add_argument('--obs_ssv', required=True, help='Input SSV file with data')
     parser.add_argument('--param_plots', action='store_true', help='Generate parameter plots')
-    parser.add_argument('--state_plots', action='store_true', help='Generate state plots')
+    parser.add_argument('--state_plot', action='store_true', help='Generate state plot')
+    parser.add_argument('--verbose', action='store_true', help='Verbose output')
     args = parser.parse_args()
     return vars(args)
 
@@ -48,23 +49,43 @@ def main():
     cli_args = read_cli_args()
     out_dir = cli_args["out"]
     param_names = ['lnV0', 'beta', 'p', 'c', 'gamma']
+    if cli_args['verbose']:
+        print(f"Reading instances from TOML file: {cli_args['input_toml']}")
     inst = list(pypfilt.load_instances(cli_args["input_toml"]))[0]
+    if cli_args['verbose']:
+        print(f"Reading observations from SSV file: {cli_args['obs_ssv']}")
     inst.settings['observations']['V']['file'] = cli_args['obs_ssv']
     inf_ctx = inst.build_context()
+    if cli_args['verbose']:
+        print("Running inference")
     inf_results = tiv_run_inference(inf_ctx)
 
     if cli_args['param_plots']:
+        if cli_args['verbose']:
+            print("Generating parameter plots")
         end_time = inf_results['end_time']
         mrgs = inf_results['marginals']
         pst_param_df = inf_results['posterior_param_df']
         for param in param_names:
+            if cli_args['verbose']:
+                print(f"\tGenerating plot for parameter: {param}")
             dd = pst_param_df[pst_param_df['name'] == param]
             plt_df = plottable_model_cis(dd)
             param_p9 = param_plt_p9(plt_df, None, mrgs[param],
                                     param)
             param_p9.save(f"{out_dir}/demo-param-{param}-histogram.png",
                           height = 5.8, width = 8.3)
-    print("hello world")
+
+    if cli_args['state_plot']:
+        if cli_args['verbose']:
+            print("Generating state plot")
+        pst_state_df = inf_results['posterior_state_df']
+        plt_df = pst_state_df[pst_state_df['name'] == 'V']
+        plt_df_obs = pd.read_csv(cli_args['obs_ssv'], sep = ' ')
+        plt_df_obs['y'] = 10**plt_df_obs['value']
+        state_p9 = state_plt_p9(plt_df, plt_df_obs)
+        state_p9.save(f"{out_dir}/demo-state-trajectory.png",
+                      height = 4.1, width = 5.8)
 
 if __name__ == "__main__":
     main()
