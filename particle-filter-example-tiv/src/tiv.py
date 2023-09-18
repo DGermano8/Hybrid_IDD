ada205754dbd0659dcc40aee7bdfca3590cd7e8e
Alex Zarebski <azarebski@6200l-214217-l.mobility.unimelb.net.au>
2023-09-18 14:55:48 +1000

    include a model to use the jsf simulator

--- particle-filter-example-tiv/src/tiv.py
+++ particle-filter-example-tiv/src/tiv.py
@@ -4,6 +4,113 @@ import pypfilt                  # type: ignore
 from pypfilt.model import Model # type: ignore
 from pypfilt.obs import Univariate, Obs # type: ignore
 import pdb
+import src.JSF_Solver_BasePython as JSF
+
+class TIV_JSF(Model):
+    """
+    """
+
+    num_particles = -1
+    threshold = 50
+    _nu_reactants = [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ]
+    _nu_products = [
+        [0, 1, 1],
+        [0, 0, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ]
+
+    _nu = [[a - b for a, b in zip(r1, r2)]
+           for r1, r2 in zip(_nu_products, _nu_reactants)]
+    _stoich = {'nu': _nu,
+               'DoDisc': [0, 1, 1],
+               'nuReactant': _nu_reactants,
+               'nuProduct': _nu_products}
+
+    def field_types(self, ctx):
+        """
+        """
+        return [
+            ('lnV0', np.float_),
+            ('beta', np.float_),
+            ('p', np.float_),
+            ('c', np.float_),
+            ('gamma', np.float_),
+            ('T', np.float_),
+            ('I', np.float_),
+            ('V', np.float_),
+            ('next_event', np.int_),
+            ('next_time', np.float_),
+        ]
+
+    def init(self, ctx, vec):
+        """
+        """
+        prior = ctx.data['prior']
+        self.num_particles = prior['T0'].shape[0]
+
+        vec['beta'] = prior['beta']
+        vec['p'] = prior['p']
+        vec['lnV0'] = prior['lnV0']
+        vec['c'] = prior['c']
+        vec['gamma'] = prior['gamma']
+        vec['T'] = prior['T0']
+        vec['I'] = prior['I0']
+        vec['V'] = np.exp(vec['lnV0'])
+
+    def _rates(self, x, theta, time):
+        """
+        """
+        t = x[0]
+        i = x[1]
+        v = x[2]
+        m_beta = theta[0]
+        m_p = theta[1]
+        m_c = theta[2]
+        m_gamma = theta[3]
+        return [m_beta*(t*v),
+                m_gamma*i,
+                m_p*i,
+                m_c*v]
+
+    def update(self, ctx, time_step, is_forecast, prev, curr):
+        """
+        """
+        _my_opts = {'EnforceDo': [0, 0, 0],
+                    'dt': time_step.dt,
+                    'SwitchingThreshold': [self.threshold,
+                                           self.threshold,
+                                           self.threshold]}
+
+        for p_ix in range(self.num_particles):
+
+            ptcl = prev[p_ix].copy()
+            x0 = [ptcl['T'], ptcl['I'], ptcl['V']]
+            theta = [
+                ptcl['beta'],
+                ptcl['p'],
+                ptcl['c'],
+                ptcl['gamma'],
+            ]
+
+            pdb.set_trace()
+            xs, ts = JSF.JumpSwitchFlowSimulator(
+                x0,
+                lambda x, time: self._rates(x, theta, time),
+                self._stoich,
+                time_step.dt,
+                _my_opts
+            )
+
+            ptcl['T'] = xs[0][-1]
+            ptcl['I'] = xs[1][-1]
+            ptcl['V'] = xs[2][-1]
+            curr[p_ix] = ptcl
 
 class TIV_ODE(Model):
     """
